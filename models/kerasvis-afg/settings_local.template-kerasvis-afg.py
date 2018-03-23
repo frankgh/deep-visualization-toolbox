a03a3038628c6afad416f31861ae5ea5af850af5
Francisco Guerrero <frank.guerrero@gmail.com>
2018-03-23 16:06:57 -0400

    Updating settings local

--- models/kerasvis-afg/settings_local.template-kerasvis-afg.py
+++ models/kerasvis-afg/settings_local.template-kerasvis-afg.py
@@ -4,17 +4,15 @@
 # will override those defined in settings.py
 
 import numpy as np
-from scipy import signal
 
 from wave_detection.spindle import DetectSpindle
-
 
 window_panes = (
     # (i, j, i_size, j_size)
     ('input', (0, 0, 300, 300)),  # This pane is required to show the input picture
-    ('input_label', (300, 0, 100, 300)),
-    ('kerasvis_aux', (400, 0, 300, 300)),
-    ('kerasvis_back', (600, 0, 300, 300)),
+    ('input_label', (300, 0, 50, 300)),
+    ('kerasvis_aux', (350, 0, 300, 300)),
+    ('kerasvis_back', (650, 0, 250, 300)),
     ('kerasvis_status', (900, 0, 30, 1500)),
     ('kerasvis_control', (0, 300, 90, 900)),
     ('kerasvis_layers', (90, 300, 810, 900)),
@@ -22,8 +20,9 @@ window_panes = (
 )
 
 # static_files_dir = '%DVT_ROOT%/models/kerasvis-afg/patients'
-static_files_dir = '%DVT_ROOT%/models/kerasvis-afg/patients_subset'
+# static_files_dir = '%DVT_ROOT%/models/kerasvis-afg/patients_subset'
 # static_files_dir = '%DVT_ROOT%/models/kerasvis-afg/patients_s2'
+static_files_dir = '%DVT_ROOT%/models/kerasvis-afg/patients_wavelet'
 
 #  Fpz-Cz <- location on the head and EEG is measuring potential in electricity
 signal_labels = ['EEG Fpz-Cz',
@@ -42,13 +41,13 @@ kerasvis_deploy_model = '%DVT_ROOT%/models/kerasvis-afg/model.h5'
 kerasvis_label_layers = ('fc8', 'prob')
 kerasvis_prob_layer = 'prob'
 kerasvis_unit_jpg_dir = '%DVT_ROOT%/models/caffenet-yos/unit_jpg_vis'
-kerasvis_jpgvis_layers = ['conv1', 'conv3', 'conv5', 'activation_9', 'activation_16', 'activation_17', 'activation_18',
-                          'dense_3']
+kerasvis_jpgvis_layers = []
 kerasvis_jpgvis_remap = {'activation_1': 'conv1', 'activation_3': 'conv3', 'activation_5': 'conv5'}
+
 
 def signal_filter_fn(data):
     detsp = DetectSpindle(method='Wamsley2012', frequency=(11, 16))
-    sp = detsp(data[:,0:2])
+    sp = detsp(data[:, 0:2])
 
     # print ('Events found', len(sp.events))
     if len(sp.events) == 0:
@@ -60,7 +59,7 @@ def signal_filter_fn(data):
         start, end, channels = int(ev['start'] * 100), int(ev['end'] * 100), ev['chan']
         for ch in channels.split(','):
             ch = int(ch)
-            filtered[start:end, ch] = np.zeros(end-start)
+            filtered[start:end, ch] = np.zeros(end - start)
             if ch not in markers:
                 markers[ch] = []
             markers[ch].append(start)
@@ -69,11 +68,18 @@ def signal_filter_fn(data):
 
 
 def static_files_data_fn(data):
+    # x_raw = data['X'][:,6000:9000,0]
+    # x_scaled = (x_raw + 199) / 399
+    # return x_scaled
     return data['X']
 
 
 def static_files_labels_fn(data):
-    return data['Y']
+    return data['Y'] if 'Y' in data else None
+
+
+def static_files_extra_fn(data):
+    return data['extra'] if 'extra' in data else None
 
 
 def kerasvis_layer_pretty_name_fn(name):
